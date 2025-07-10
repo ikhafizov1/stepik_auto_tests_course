f1c6f89708b441ea1f8aafce60773c795f631605
unknown <zero-sq@rambler.ru>
2025-07-10 11:15:47 +0300

    Смотрим работу конструкции with pytest.raises(), например, можно проверить, что на странице сайта не должен отображаться какой-то элемент

---
+++ NoSuchElement.py
@@ -0,0 +1,30 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+'''
+Тест найдет кнопку и упадет, что обнаружил кнопку.
+
+def test_exception1():
+    try:
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/")
+        with pytest.raises(NoSuchElementException):
+            browser.find_element(By.CSS_SELECTOR, "button.btn")
+            pytest.fail("Не должно быть кнопки Отправить")
+    finally: 
+        browser.quit()
+'''
+# Тест будет выполнен, т.к. кнопка Отправить необнаружена
+
+def test_exception2():
+    try:
+        browser = webdriver.Chrome()
+        browser.get("http://selenium1py.pythonanywhere.com/")
+        with pytest.raises(NoSuchElementException):
+            browser.find_element(By.CSS_SELECTOR, "no_such_button.btn")
+            pytest.fail("Не должно быть кнопки Отправить")
+    finally: 
+        browser.quit()
